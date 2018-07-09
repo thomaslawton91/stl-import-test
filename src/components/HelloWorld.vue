be63989ec59857fa919b280df189fa9a9b3fb0c5
thomaslawton91 <thomaslawton91@gmail.com>
2018-07-09 01:42:41 -0400

    working

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <model-stl src="/cube.stl"></model-stl>
-  </div>
+  <renderer :size="{ w:600, h: 600}">
+    <scene>
+      <camera :position="{ z: 100 }"></camera>
+      <mesh :obj="mesh" :position="{ y: -200 }"></mesh>
+      <animation :fn="animate" :speed="3"></animation>
+    </scene>
+  </renderer>
 </template>
 
 <script>
@@ -9,6 +13,23 @@ import { ModelStl } from 'vue-3d-model'
 export default {
   name: 'HelloWorld',
   components: { ModelStl },
+  data: function () {
+    return {
+      mesh: null
+    }
+  },
+  methods: {
+    importedStl() {
+      var THREE = require ('three');
+      var STLLoader = require('three-stl-loader')(THREE);
+      var loader = new STLLoader();
+      return loader.load('/cube.stl', function(geometry){
+        var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        var mesh = new THREE.Mesh(geometry, material);
+        this.mesh = mesh
+      })
+    },
+  }
 }
 </script>
 
